d4580879b0158e649bf4b75517b86f7a5ac60c49
Vanessa Goncalez <vanessagoncalez@hotmail.com>
2022-04-02 15:43:07 -0300

    Finalização Modulo I

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,9 +1,9 @@
-module.exports.sum = (num1, num2) => {
-	const int1 = parseInt(num1, 10);
-	const int2 = parseInt(num2, 10);
+export function sum(num1, num2) {
+  const int1 = parseInt(num1, 10);
+  const int2 = parseInt(num2, 10);
 
-	if (Number.isNaN(int1) || Number.isNaN(int2)){
-		throw new Error('Please check your input')
-	}
-	return +num1 + +num2;
+  if (Number.isNaN(int1) || Number.isNaN(int2)) {
+    throw new Error('Please check your input');
+  }
+  return +num1 + +num2;
 }
